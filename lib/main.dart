b934c08b0a105e93729f741d0d2d4b2289933c88
usho64k <usuallybreadspace@gmail.com>
2021-04-22 17:05:11 +0900

    with command 2(flutter packages get) & (dart pub get)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -22,7 +24,10 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Welcome to Flutter'),),
+        body:  Center(child: Text(wordPair.asPascalCase),),
+      )
     );
   }
 }
